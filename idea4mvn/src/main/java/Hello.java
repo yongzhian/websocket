1b120bb4a3d3820015f91040bd57fa989ed21ac2
yongzhian <yongzhian@163.com>
2016-07-09 14:37:20 +0800

    添加测试代码

---
+++ idea4mvn/src/main/java/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Zain on 2016/7/9.
+ */
+public class Hello {
+    public void sayHello(){
+        System.out.println("hello word");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 12; i++) {
+            
+        }
+    }
+}
